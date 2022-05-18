38a662d748bf25023b0a5a700d569ee2425e3eba
Mark Thurston <mark.thurston@nhs.net>
2022-05-18 16:08:00 +0100

    Linted

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,13 +1,23 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
-
+from cohortextractor import codelist  # NOQA
+from cohortextractor import StudyDefinition, patients
 
 study = StudyDefinition(
     default_expectations={
-        "date": {"earliest": "1900-01-01", "latest": "today"},
+        "date": {
+            "earliest": "1900-01-01",
+            "latest": "today"
+        },
         "rate": "uniform",
         "incidence": 0.5,
     },
     population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
-    ),
-)
+        "2019-02-01", "2020-02-01"),
+    age=patients.age_as_of(
+        "2019-09-01",
+        return_expectations={
+            "rate": "universal",
+            "int": {
+                "distribution": "population_ages"
+            },
+        },
+    ))
